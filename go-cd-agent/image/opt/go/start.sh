fe3ba6939c4f2eda7271238a5e89c0a79948f31d
Karol Wojtaszek <karol@appunite.com>
2015-01-20 00:53:07 +0100

    add ssh agent

--- go-cd-agent/image/opt/go/start.sh
+++ go-cd-agent/image/opt/go/start.sh
@@ -1,5 +1,7 @@
 mkdir /usr/share/go-agent/config
 
+eval `ssh-agent -s`
+
   cat<<EOF > /usr/share/go-agent/config/autoregister.properties
 agent.auto.register.key=$AGENT_KEY
 agent.auto.register.resources=$AGENT_RESOURCES
